b73c5411d9a07841608c8021f238f3c06fd4b2ec
Natalie Bolón <natalie.bolonbrun@epfl.ch>
2019-05-13 23:40:35 +0200

    Adam optimizer

--- Project2/Optimizers.py
+++ Project2/Optimizers.py
@@ -47,5 +47,40 @@ class DecreaseSGD(Optimizers):
 
     def param(self):
         return []
+    
+    
+    
 
+class Adam(Optimizers):
+    def __init__(self, eta, beta1, beta2, delta):
+        Optimizers.__init__(self)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eta = eta
+        self.delta = delta
+        self.m1_w = None
+        self.m2_w = None
+        
+
+    def __call__(self, *input):
+        if self.m1_w is None:
+            self.m1_w = [torch.zeros(l.gradwrtweight.size()) for l in input[0] if isinstance(l, Linear)]
+            
+        if self.m2_w is None:
+            self.m2_w = [torch.zeros(l.gradwrtweight.size()) for l in input[0] if isinstance(l, Linear)]  
+        
+        i = 0
+        for l in input[0]:
+            if isinstance(l, Linear):
+                self.m1_w[i] = self.beta1 * self.m1_w[i] + (1 - self.beta1) * l.gradwrtweight
+                self.m2_w[i] = self.beta2 * self.m2_w[i] + (1 - self.beta2) * l.gradwrtweight * l.gradwrtweight
+                
+                m1_w = self.m1_w[i] / (1 - self.beta1 ** (i + 1))
+                m2_w = self.m2_w[i] / (1 - self.beta2 ** (i + 1))
+                
+                l.weight -= self.eta * m1_w / (self.delta + torch.sqrt(m2_w))
+                l.bias -= self.eta*l.gradwrtbias
+                i += 1
+
+               
 
